9b217eadf26937ce8e6179229d48cb771f4e5053
Leonardo Custodio <44417666+leoskiline@users.noreply.github.com>
2021-05-05 16:07:37 -0300

    upd

---
+++ Arvores/Arvore Huffman/e1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct tree
+{
+	
+	struct tree *esq;
+	struct tree *dir;
+}typedef Tree;
+
+int main()
+{
+	char string[100];
+	strcpy(string,"Amo como ama o amor. Não conheço nenhuma outra razão para amar senão amar. Que queres que te diga, além de que te amo, se o que quero dizer-te é que te amo?");
+	
+	return 0;
+}
